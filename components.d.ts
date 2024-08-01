da08e5e2afa2b4d8970c34cf2842a74cb11a3288
D.q._Fan <74292094+glacier-bot@users.noreply.github.com>
2024-08-02 01:06:42 +0800

    测试版v1
    删除了没用的文件，保证项目顺利打包，减小部署的体积

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    AMap: typeof import('./src/components/AMap.vue')['default']
     AMapReConstruct: typeof import('./src/components/AMapReConstruct.vue')['default']
     ArrowButtons: typeof import('./src/components/buttons/ArrowButtons.vue')['default']
     AsideItem: typeof import('./src/components/AsideItem.vue')['default']
@@ -33,17 +32,9 @@ declare module 'vue' {
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElText: typeof import('element-plus/es')['ElText']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     PropertyBoxPolygon: typeof import('./src/components/dialogs/PropertyBoxPolygon.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SwitchButtons: typeof import('./src/components/buttons/SwitchButtons.vue')['default']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
